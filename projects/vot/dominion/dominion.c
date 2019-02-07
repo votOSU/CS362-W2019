12810d12115aca481020ea237850b576d305d952
Thach Vo <vot@flip2.engr.oregonstate.edu>
2019-02-06 17:37:53 -0800

    assginment 2 change

--- projects/vot/dominion/dominion.c
+++ projects/vot/dominion/dominion.c
@@ -667,7 +667,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      while(drawntreasure<2){
+     /* while(drawntreasure<2){
 	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
 	  shuffle(currentPlayer, state);
 	}
@@ -684,12 +684,13 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       while(z-1>=0){
 	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
 	z=z-1;
-      }
+      }*/
+	adventurerRefactor(state); //calling the rafactoring function 
       return 0;
 			
     case council_room:
       //+4 Cards
-      for (i = 0; i < 4; i++)
+      /*for (i = 0; i < 4; i++)
 	{
 	  drawCard(currentPlayer, state);
 	}
@@ -707,8 +708,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 	}
 			
       //put played card in played card pile
-      discardCard(handPos, currentPlayer, state, 0);
-			
+      discardCard(handPos, currentPlayer, state, 0);*/
+	council_roomRefactor(state, handPos);		
       return 0;
 			
     case feast:
@@ -830,13 +831,14 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 		
     case smithy:
       //+3 Cards
-      for (i = 0; i < 3; i++)
+     /* for (i = 0; i < 3; i++)
 	{
 	  drawCard(currentPlayer, state);
 	}
 			
       //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
+      discardCard(handPos, currentPlayer, state, 0);*/
+	smithyRefactor(state, handPos);
       return 0;
 		
     case village:
@@ -903,13 +905,14 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 		
     case great_hall:
       //+1 Card
-      drawCard(currentPlayer, state);
+      /*drawCard(currentPlayer, state);
 			
       //+1 Actions
       state->numActions++;
 			
       //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
+      discardCard(handPos, currentPlayer, state, 0);*/
+	great_hallRefactor(state, handPos);
       return 0;
 		
     case minion:
@@ -1140,7 +1143,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 		
     case embargo: 
       //+2 Coins
-      state->coins = state->coins + 2;
+     /* state->coins = state->coins + 2;
 			
       //see if selected pile is in play
       if ( state->supplyCount[choice1] == -1 )
@@ -1152,7 +1155,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       state->embargoTokens[choice1]++;
 			
       //trash card
-      discardCard(handPos, currentPlayer, state, 1);		
+      discardCard(handPos, currentPlayer, state, 1);*/
+	embargoRefactor(choice1, state, handPos);		
       return 0;
 		
     case outpost:
@@ -1326,6 +1330,116 @@ int updateCoins(int player, struct gameState *state, int bonus)
   state->coins += bonus;
 
   return 0;
+}
+
+/*** REFACTOR SECTION ***/
+
+int adventurerRefactor(struct gameState *state)
+{
+  int currentPlayer = whoseTurn(state);
+  int temphand[MAX_HAND];
+  int drawntreasure=0;
+  int cardDrawn;
+  int z = 0;  // counter 
+
+  while(drawntreasure<1) /******** BUG introducing  <1 instead of origin <2*********/
+  {   
+    if (state->deckCount[currentPlayer] <1)
+	{//if the deck is empty we need to shuffle discard and add to deck
+
+	  shuffle(currentPlayer, state);
+
+	}
+	drawCard(currentPlayer, state);
+	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+
+	  drawntreasure++;
+
+	else
+	{
+	  temphand[z]=cardDrawn;
+	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+	  z++;
+	}
+  }
+  while(z-1>=0)
+  {
+	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+	z=z-1;
+  }
+  return 0;
+}
+
+int smithyRefactor(struct gameState *state, int handPos)
+{
+	int i;
+	int currentPlayer = whoseTurn(state);
+  	//+3 Cards
+  	for (i = 0; i < 3; i++)
+  	{
+  		drawCard(currentPlayer, state);
+  	}
+	discardCard(handPos, currentPlayer, state, 1);    /******** BUG introducing  1 instead of origin 0 *********/
+  	return 0;
+}
+
+
+int council_roomRefactor(struct gameState *state, int handPos)
+{
+	int i;
+	int currentPlayer = whoseTurn(state);
+	for (i = 0; i < 4; i++)
+	{
+		drawCard(currentPlayer, state);
+	}
+ 	 //+1 Buy
+ 	state->numBuys++;
+  	  //Each other player draws a card
+    	for (i = 0; i < state->numPlayers; i++)
+  	{
+  		if ( i == currentPlayer )  /******** BUG introducing  == instead of origin != *********/
+  		{
+  			drawCard(i, state);
+  		}
+  	}
+	//put played card in played card pile
+	discardCard(handPos, currentPlayer, state, 0);
+	return 0;
+}
+
+
+int great_hallRefactor(struct gameState *state, int handPos)
+{     
+	int currentPlayer = whoseTurn(state);
+	//+1 Card
+	drawCard(currentPlayer, state);
+	     			
+	     //+1 Actions
+        state->numActions++;
+	    			            			
+	      //discard card from hand
+        discardCard(handPos, currentPlayer, state, 0);
+        return 0;
+}
+
+int embargoRefactor(int choice1, struct gameState *state, int handPos)
+{      
+	int currentPlayer = whoseTurn(state);
+	state->coins = state->coins + 1; /******** BUG introducing  1 instead of origin 2 *********/
+			
+      //see if selected pile is in play
+      if ( state->supplyCount[choice1] == -1 )
+      {
+      		  return -1;
+      }
+           		  				
+	    //add embargo token to selected supply pile
+      state->embargoTokens[choice1]++;
+     		  				            			
+	    //trash card
+      discardCard(handPos, currentPlayer, state, 1);		
+      return 0;
 }
 
 
